5bb4526d03f63c16bfaf054ccf53b489946d2e9e
PeterPark07 <128113413+PeterPark07@users.noreply.github.com>
2023-06-06 22:56:22 +0530

    Update main.py

--- main.py
+++ main.py
@@ -25,53 +25,32 @@ def telegram():
 
 @bot.message_handler(func=lambda message: True)
 def images(message):
-    input_text = message.text.split()[0]
+    input_text = message.text.replace(' ', '_')
 
     local_url = url + f'index.php?page=post&s=list&tags={input_text}'
     response = requests.get(local_url, headers=headers)
 
-    # Step 4: Check if the request was successful (status code 200)
     if response.status_code == 200:
-        # Step 5: Create a BeautifulSoup object to parse the HTML
         soup = BeautifulSoup(response.text, 'html.parser')
-
         links = ""
-        counter = 0  # Counter variable for limiting the number of links
+        counter = 10
 
-        # Step 5: Find all the <a> tags in the HTML
         a_tags = soup.find_all('a')
 
-        # Step 6: Extract the href attribute from each <a> tag and display it
         for a in a_tags:
             href = a.get('href')
             if 's=view' in href:
                 absolute_url = requests.compat.urljoin(url, href)
                 links += absolute_url
                 links += '\n'
-                counter += 1  # Increment the counter
-            if counter == 10:  # Break the loop when counter reaches 10
+                counter -= 1
+            if counter == 0:
                 break
+
         if links != "":
-            sent_message = bot.reply_to(message, links)
-            message_ids.append(sent_message.message_id)
-            images = []
-            for i in links.splitlines():
-                # Send a request to the absolute URL
-                img_response = requests.get(i, headers=headers)
-                if img_response.status_code == 200:
-                    img_soup = BeautifulSoup(img_response.text, 'html.parser')
-
-                    # Find all <img> tags with id="image"
-                    img_tags = img_soup.find_all('img', id='image')
-                    for img in img_tags:
-                        # Remove the query string from the image URL
-                        img_src = img['src'].split('?', 1)[0]
-
-                        images.append(img_src)
+            images = get_image_urls(links)
             if len(images) != 0:
-                for img_url in images:
-                    sent_message = bot.send_photo(message.chat.id, img_url)
-                    message_ids.append(sent_message.message_id)  # Store the message ID
+                send_images(message.chat.id, images)
             else:
                 bot.reply_to(message, "No results")
         else:
@@ -79,9 +58,30 @@ def images(message):
     else:
         bot.reply_to(message, "Failed to fetch website")
 
-    # Schedule the deletion of sent messages after 10 seconds
+    schedule_message_deletion()
+
+    
+def get_image_urls(links):
+    images = []
+    for i in links.splitlines():
+        img_response = requests.get(i, headers=headers)
+        if img_response.status_code == 200:
+            img_soup = BeautifulSoup(img_response.text, 'html.parser')
+            img_tags = img_soup.find_all('img', id='image')
+            for img in img_tags:
+                img_src = img['src'].split('?', 1)[0]
+                images.append(img_src)
+    return images
+
+
+def send_images(chat_id, images):
+    for img_url in images:
+        sent_message = bot.send_photo(chat_id, img_url)
+        message_ids.append(sent_message.message_id)
+
+        
+def schedule_message_deletion():
     time.sleep(10)
     for message_id in message_ids:
         bot.delete_message(message.chat.id, message_id)
-    message_ids.clear()  # Clear the list of message IDs
-
+    message_ids.clear()
