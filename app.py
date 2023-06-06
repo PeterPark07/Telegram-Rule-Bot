9ab4a7e8be0be0e1512055325beea8d9bb218724
PeterPark07 <128113413+PeterPark07@users.noreply.github.com>
2023-06-06 20:33:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ url = os.getenv('url')
 # Step 2: Send a request to the website and get the HTML content
 response = requests.get(url)
 
-for i in range 20:  
+for i in range(20):  
   # Step 3: Check if the request was successful (status code 200)
   if response.status_code == 200:
       # Step 4: Print the content of the website
