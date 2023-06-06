e5dc1f11442482aee7fa33a81f659fa792b692ad
PeterPark07 <128113413+PeterPark07@users.noreply.github.com>
2023-06-06 20:39:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,9 +2,14 @@ import requests
 import os
 # Step 1: Get the URL from the user
 url = os.getenv('url')
+# Step 2: Set the headers
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.150 Safari/537.36',
+    'Accept-Language': 'en-US,en;q=0.9',
+}
 
-# Step 2: Send a request to the website and get the HTML content
-response = requests.get(url)
+# Step 3: Send a request to the website and get the HTML content
+response = requests.get(url, headers=headers)
 
 for i in range(20):  
   # Step 3: Check if the request was successful (status code 200)
