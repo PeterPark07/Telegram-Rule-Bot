c6a358d1965ba5c92a8339604a1788723460836c
PeterPark07 <128113413+PeterPark07@users.noreply.github.com>
2023-06-07 14:15:36 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 bot = telebot.TeleBot(os.getenv('bot'), threaded=False)
 url = os.getenv('url')
 number_images = 15
-modes = [[2,0.2], [5, 0.5] , [20, 2 ] , [60 , 5 ]]
+modes = [[2, 0.2], [5, 0.5], [20, 2], [60, 5]]
 mode = modes[0]
 last_message_id = None
 
@@ -27,11 +27,11 @@ def telegram():
         bot.process_new_updates([telebot.types.Update.de_json(request.get_data().decode('utf-8'))])
         return 'OK', 200
 
-    
+
 @bot.message_handler(commands=['settings'])
 def handle_settings(message):
     markup = telebot.types.InlineKeyboardMarkup()
-    
+
     # Add button to change number_images
     number_images_options = ['2 images', '5 images', '10 images', '20 images', '30 images', '40 images']
     number_images_buttons = []
@@ -39,13 +39,16 @@ def handle_settings(message):
         number_images_buttons.append(telebot.types.InlineKeyboardButton(str(option), callback_data=f"num{option}"))
     markup.row(*number_images_buttons[0:3])
     markup.row(*number_images_buttons[3:6])
+    
     # Add button to change mode
-    mode_options = [("Mode 1 (timeout = 2 seconds)", 1), ("Mode 2 (timeout = 5 seconds)", 2), ("Mode 3 (timeout = 20 seconds)", 3), ("Mode 4 (timeout = 60 seconds)", 4)]
+    mode_options = [("Mode 1 (timeout = 2 seconds)", 1), ("Mode 2 (timeout = 5 seconds)", 2),
+                    ("Mode 3 (timeout = 20 seconds)", 3), ("Mode 4 (timeout = 60 seconds)", 4)]
     mode_buttons = []
     for option in mode_options:
         mode_buttons.append(telebot.types.InlineKeyboardButton(option[0], callback_data=f"mode{option[1]}"))
     markup.row(*mode_buttons[0:2])
     markup.row(*mode_buttons[2:4])
+    
     bot.send_message(message.chat.id, "Choose an option:", reply_markup=markup)
 
 
@@ -55,22 +58,22 @@ def handle_callback_query(call):
         # Callback for changing number_images
         if call.data.startswith("num"):
             global number_images
-            number_images = int((call.data.replace("num" , '').split()[0])
+            number_images = int(call.data.replace("num", '').split()[0])
             bot.answer_callback_query(call.id, f"Number of images set to {number_images}")
             bot.send_message(call.message.chat.id, f"Number of images changed to {number_images}")
-        
+
         # Callback for changing mode
         elif call.data.startswith("mode"):
-            mode_info = int(call.data.replace('mode',''))
+            mode_info = int(call.data.replace('mode', ''))
             global mode
             mode = modes[mode_info - 1]
             bot.answer_callback_query(call.id, f"Mode changed to {mode_info}")
             bot.send_message(call.message.chat.id, f"Mode changed to {mode_info}")
-        
+
         # Edit the original message to remove the inline keyboard
         bot.edit_message_reply_markup(call.message.chat.id, call.message.message_id)
 
-    
+
 @bot.message_handler(func=lambda message: True)
 def images(message):
     global last_message_id
@@ -81,11 +84,11 @@ def images(message):
 
     # Store the current message ID as the most recent one
     last_message_id = message.message_id
-    
+
     input_text = message.text.replace(' ', '_')
-    
+
     local_url = get_local_url(input_text)
-        
+
     response = requests.get(local_url, headers=headers)
 
     if response.status_code == 200:
@@ -99,13 +102,13 @@ def images(message):
     else:
         bot.reply_to(message, "Failed to fetch website")
 
-    schedule_message_deletion(message, message_ids , mode)
+    schedule_message_deletion(message, message_ids, mode)
     return
 
 
 def get_local_url(input_text):
     if input_text.startswith('/more'):
-        input_text = input_text.replace('/more','')
+        input_text = input_text.replace('/more', '')
         num = int(input_text[0]) * number_images
         input_text = input_text[2:]
         local_url = url + f'index.php?page=post&s=list&tags={input_text}&pid={num}'
@@ -131,6 +134,7 @@ def get_links(counter, response):
             break
     return links
 
+
 def get_image_urls(links):
     images = []
     for i in links.splitlines():
@@ -149,7 +153,7 @@ def send_images(chat_id, images, message_ids):
         sent_message = bot.send_photo(chat_id, img_url)
         message_ids.append(sent_message.message_id)
 
-        
+
 def schedule_message_deletion(message, message_ids, mode):
     time.sleep(mode[0])
     for message_id in message_ids:
