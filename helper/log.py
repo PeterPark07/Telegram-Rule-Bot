cd1384850c04a18d4fe680b60bbe3412ef1cfdbb
PeterPark07 <128113413+PeterPark07@users.noreply.github.com>
2023-06-07 20:43:35 +0530

    Update log.py

--- helper/log.py
+++ helper/log.py
@@ -1,54 +1,21 @@
-import os 
+import os
 
-  
+log_chat = os.getenv('log_chat')
 
-log_chat = os.getenv('log_chat') 
+def send_log(bot, message):
+    user_id = message.from_user.id
+    chat_id = message.chat.id
+    message_text = message.text
+    name = message.from_user.username or message.from_user.first_name
 
-admin_user = int(os.getenv('admin')) 
+    # Determine the information to display based on user and chat IDs
+    elif user_id == chat_id:
+        log_info = f"🔸 User: {name} \nChat ID: {chat_id}\n"
+    else:
+        log_info = f"🔸 User: {name} \nUser ID: {user_id}\nChat ID: {chat_id}\n"
 
-users = [int(user_id) for user_id in os.getenv('users').split(',')] 
+    # Construct the log message with relevant details
+    log_message = f"🤖 Bot: @{bot.get_me().username}\n{log_info}Message: {message_text}"
 
-  
-
-def send_log(bot, message): 
-
-     user_id = message.from_user.id 
-
-     chat_id = message.chat.id 
-
-     message_text = message.text 
-
-     name = message.from_user.username or message.from_user.first_name 
-
-  
-
-     # Determine the information to display based on user and chat IDs 
-
-     if user_id == admin_user: 
-
-         log_info = "⭐️ ADMIN ⭐️\n" 
-
-     elif user_id in users: 
-
-         log_info = f"🔹 User: {name} (USER)\nUser ID: {user_id}\n" 
-
-     elif user_id == chat_id: 
-
-         log_info = f"🔸 User: {name} \nChat ID: {chat_id}\n" 
-
-     else: 
-
-         log_info = f"🔸 User: {name} \nUser ID: {user_id}\nChat ID: {chat_id}\n" 
-
-  
-
-     # Construct the log message with relevant details 
-
-     log_message = f"🤖 Bot: @{bot.get_me().username}\n{log_info}Message: {message_text}" 
-
-  
-
-     # Send the log message to the designated log chat 
-
-     bot.send_message(log_chat, log_message)
-      
+    # Send the log message to the designated log chat
+    bot.send_message(log_chat, log_message)
